458a7578983135454e26d7efd7b7b1bb5fafb953
Howard M. Harte <hharte@magicandroidapps.com>
2021-02-13 20:15:41 -0800

    nsdos: Utility for North Star DOS Disk Images
    nsdos can list the contents of a North Star DOS disk image, and
    optionally extract the files from the disk image.

---
+++ README.md
@@ -0,0 +1,201 @@
+# nsdos - North Star DOS Disk Utility
+
+North Star Computers had their own DOS for use with their Horizon and Advantage computer systems, as well as for use with their disk controller.  These disks have a very simple file system, where every file has to be contiguous.
+
+`nsdos` can list and Extract Files from North Star disk images in Dave Dunfield’s [.nsi format](http://dunfield.classiccmp.org/img/). The .nsi format is commonly used in the [SIMH/AltairZ80](https://schorn.ch/altair.html) simulator, as well as with [FluxEngine for North Star](https://github.com/hharte/fluxengine/tree/northstar).
+
+The filename in North Star DOS can be up to eight characters.  Each file has an associated file type, from 0-127, although only four are currently defined:
+
+
+<table>
+  <tr>
+   <td>Type
+   </td>
+   <td>Description
+   </td>
+   <td>Metadata
+   </td>
+   <td>Output Filename
+   </td>
+  </tr>
+  <tr>
+   <td>0
+   </td>
+   <td>Default
+   </td>
+   <td>unused
+   </td>
+   <td>fname.DEFAULT
+   </td>
+  </tr>
+  <tr>
+   <td>1
+   </td>
+   <td>Binary Object Code
+   </td>
+   <td>Load Address
+   </td>
+   <td>fname.OBJECT_L&lt;load_address>
+   </td>
+  </tr>
+  <tr>
+   <td>2
+   </td>
+   <td>BASIC source file
+   </td>
+   <td>Actual File Size
+   </td>
+   <td>fname.BASIC
+   </td>
+  </tr>
+  <tr>
+   <td>3
+   </td>
+   <td>BASIC data file
+   </td>
+   <td>unused
+   </td>
+   <td>fname.BASIC_DATA
+   </td>
+  </tr>
+</table>
+
+
+In addition to the file name and type, the North Star DOS filesystem stores metadata about certain types of files.  For executable object code, the “load address” is stored in the metadata.  To preserve this important information, `nsdos` appends the load address to the output filename extension.
+
+
+# Usage
+
+
+```
+North Star DOS File Utility (c) 2021 - Howard M. Harte
+
+usage is: nsdos <filename.nsi> [command] [<filename>|<path>]
+        <filename.nsi> North Star DOS Disk Image in .nsi format.
+        [command]      LI - List files
+                       EX - Extract files to <path>
+If no command is given, LIst is assumed.
+```
+
+
+
+## To list files on a North Star DOS disk image:
+
+
+```
+$ nsdos <filename.nsi>
+```
+
+
+Or
+
+
+```
+$ nsdos <filename.nsi> li
+```
+
+
+For example:
+
+
+```
+$ nsdos North_Star_DOS_BASIC_2_2_1_DQ_System_Disk_PN03066A.nsi
+North Star DOS File Utility (c) 2021 - Howard M. Harte
+
+Filename  DA BLKS D TYP Type          Metadata
+DOSBASIC   0    8 D   0 Default       08,00,51
+SYSTEM     0    0 D   0 Default       00,00,00
+211DQ      0    0 D   0 Default       00,00,00
+03066A     0    0 D   0 Default       00,00,00
+03067A     0    0 D   0 Default       00,00,00
+<*>        0    8 D   3 BASIC Data    08,00,00
+DOS        4   14 D   0 Default       07,20,20
+BASIC     11   56 D   1 Object Code   Load addr: 1000
+CD        39    4 D   1 Object Code   Load addr: 1000
+CF        41    6 D   1 Object Code   Load addr: 1000
+CK        44    4 D   1 Object Code   Load addr: 1000
+CO        46    8 D   1 Object Code   Load addr: 1000
+DT        50    4 D   1 Object Code   Load addr: 1000
+M1000     52    8 D   1 Object Code   Load addr: 1000
+RAMTEST3  56    4 D   1 Object Code   Load addr: 3000
+RAMTEST5  58    4 D   1 Object Code   Load addr: 5000
+MOVER     60   18 D   2 BASIC Program Actual Size: 18
+SYSGEN    69   58 D   2 BASIC Program Actual Size: 58
+-DOS      98   14 D   3 BASIC Data    07,20,20
+-CD      105    4 D   3 BASIC Data    02,20,20
+-CF      107    6 D   3 BASIC Data    03,20,20
+-CK      110    4 D   3 BASIC Data    02,20,20
+-CO      112    8 D   3 BASIC Data    04,20,20
+-DT      116    4 D   3 BASIC Data    02,20,20
+-MONITOR 118    8 D   3 BASIC Data    04,20,20
+-BASIC   122   56 D   3 BASIC Data    38,00,00
+FPBASIC  150   54 D   1 Object Code   Load addr: 1000
+BASIC10  177   56 D   1 Object Code   Load addr: 1000
+FPBAS10  205   54 D   1 Object Code   Load addr: 1000
+BASIC12  232   58 D   1 Object Code   Load addr: 1000
+FPBAS12  261   54 D   1 Object Code   Load addr: 1000
+BASIC14  288   58 D   1 Object Code   Load addr: 1000
+FPBAS14  317   54 D   1 Object Code   Load addr: 1000
+-FPBASIC 344   54 D   3 BASIC Data    36,00,00
+-BASIC10 371   56 D   3 BASIC Data    38,00,00
+-FPBAS10 399   54 D   3 BASIC Data    36,00,00
+-BASIC12 426   58 D   3 BASIC Data    3A,00,00
+-FPBAS12 455   54 D   3 BASIC Data    36,00,00
+-BASIC14 482   58 D   3 BASIC Data    3A,00,00
+-FPBAS14 511   54 D   3 BASIC Data    36,00,00
+EQUS     538   52 D   0 Default       34,00,00
+```
+
+
+
+## To Extract All Files from the disk image:
+
+
+```
+$ nsdos <filename.nsi> ex <path>
+```
+
+
+For example:
+
+
+```
+$ ./nsdos North_Star_DOS_BASIC_2_2_1_DQ_System_Disk_PN03066A.nsi ex out
+North Star DOS File Utility (c) 2021 - Howard M. Harte
+
+     DOS -> out/DOS.DEFAULT (3584 bytes)
+   BASIC -> out/BASIC.OBJECT_L1000 (14336 bytes)
+      CD -> out/CD.OBJECT_L1000 (1024 bytes)
+      CF -> out/CF.OBJECT_L1000 (1536 bytes)
+      CK -> out/CK.OBJECT_L1000 (1024 bytes)
+      CO -> out/CO.OBJECT_L1000 (2048 bytes)
+      DT -> out/DT.OBJECT_L1000 (1024 bytes)
+   M1000 -> out/M1000.OBJECT_L1000 (2048 bytes)
+RAMTEST3 -> out/RAMTEST3.OBJECT_L3000 (1024 bytes)
+RAMTEST5 -> out/RAMTEST5.OBJECT_L5000 (1024 bytes)
+   MOVER -> out/MOVER.BASIC (4608 bytes)
+  SYSGEN -> out/SYSGEN.BASIC (14848 bytes)
+    -DOS -> out/-DOS.BASIC_DATA (3584 bytes)
+     -CD -> out/-CD.BASIC_DATA (1024 bytes)
+     -CF -> out/-CF.BASIC_DATA (1536 bytes)
+     -CK -> out/-CK.BASIC_DATA (1024 bytes)
+     -CO -> out/-CO.BASIC_DATA (2048 bytes)
+     -DT -> out/-DT.BASIC_DATA (1024 bytes)
+-MONITOR -> out/-MONITOR.BASIC_DATA (2048 bytes)
+  -BASIC -> out/-BASIC.BASIC_DATA (14336 bytes)
+ FPBASIC -> out/FPBASIC.OBJECT_L1000 (13824 bytes)
+ BASIC10 -> out/BASIC10.OBJECT_L1000 (14336 bytes)
+ FPBAS10 -> out/FPBAS10.OBJECT_L1000 (13824 bytes)
+ BASIC12 -> out/BASIC12.OBJECT_L1000 (14848 bytes)
+ FPBAS12 -> out/FPBAS12.OBJECT_L1000 (13824 bytes)
+ BASIC14 -> out/BASIC14.OBJECT_L1000 (14848 bytes)
+ FPBAS14 -> out/FPBAS14.OBJECT_L1000 (13824 bytes)
+-FPBASIC -> out/-FPBASIC.BASIC_DATA (13824 bytes)
+-BASIC10 -> out/-BASIC10.BASIC_DATA (14336 bytes)
+-FPBAS10 -> out/-FPBAS10.BASIC_DATA (13824 bytes)
+-BASIC12 -> out/-BASIC12.BASIC_DATA (14848 bytes)
+-FPBAS12 -> out/-FPBAS12.BASIC_DATA (13824 bytes)
+-BASIC14 -> out/-BASIC14.BASIC_DATA (14848 bytes)
+-FPBAS14 -> out/-FPBAS14.BASIC_DATA (13824 bytes)
+    EQUS -> out/EQUS.DEFAULT (13312 bytes)
+Extracted 35 files.
---
+++ nsdos.c
@@ -0,0 +1,271 @@
+﻿/*
+ * Utility to list directory contents of North Star DOS floppy disks,
+ * and optionally extract the files.
+ *
+ * www.github.com/hharte/nsdos
+ *
+ * (c) 2021, Howard M. Harte
+ *
+ * Reference: North Star System Software Manual Rev 2.1
+ *
+ */
+
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NS_BLOCK_SIZE		(256)
+#define DIR_ENTRIES_DD		(16 * 8)
+#define DIR_ENTRIES_SD		(16 * 4)
+#define SNAME_LEN	8
+#define DOUBLE_DENSITY_FLAG	(0x80)
+
+#define FILE_TYPE_DEFAULT		(0)
+#define FILE_TYPE_OBJECT		(1)
+#define FILE_TYPE_BASIC_SRC		(2)
+#define FILE_TYPE_BASIC_DATA	(3)
+
+const char* file_type_str[] = {
+	"Default      ",
+	"Object Code  ",
+	"BASIC Program",
+	"BASIC Data   ",
+};
+
+/* North Star DOS Directory Entry */
+typedef struct ns_dir_entry {
+	char sname[SNAME_LEN];
+	uint16_t disk_address;
+	uint16_t block_count;
+	uint8_t file_type;
+	uint8_t type_dependent_info[3];
+} ns_dir_entry_t;
+
+/* Function prototypes */
+int ns_read_dir_entries(FILE* stream, ns_dir_entry_t* dir_entries);
+void ns_list_dir_entry(ns_dir_entry_t* dir_entry);
+int ns_extract_file(ns_dir_entry_t* dir_entry, FILE* instream, char *path);
+
+#if defined(_WIN32)
+#define strncasecmp(x,y,z) _strnicmp(x,y,z)
+#endif
+
+int main(int argc, char *argv[])
+{
+	FILE* instream;
+	ns_dir_entry_t *dir_entry_list;
+	int dir_entry_cnt;
+	int i;
+	int result;
+	int extracted_file_count = 0;
+	int status = 0;
+
+	printf("North Star DOS File Utility (c) 2021 - Howard M. Harte\n\n");
+
+	if (argc < 2) {
+		printf("usage is: %s <filename.nsi> [command] [<filename>|<path>]\n", argv[0]);
+		printf("\t<filename.nsi> North Star DOS Disk Image in .nsi format.\n");
+		printf("\t[command]      LI - List files\n");
+		printf("\t               EX - Extract files to <path>\n");
+		printf("If no command is given, LIst is assumed.\n");
+		return (-1);
+	}
+
+	if (!(instream = fopen(argv[1], "rb"))) {
+		fprintf(stderr, "Error Openening %s\n", argv[1]);
+		return (-ENOENT);
+	}
+
+	dir_entry_list = (ns_dir_entry_t*)calloc(DIR_ENTRIES_DD, sizeof(ns_dir_entry_t));
+
+	if (dir_entry_list == NULL) {
+		fprintf(stderr, "Memory allocation of %d bytes failed\n", (int)(DIR_ENTRIES_DD * sizeof(ns_dir_entry_t)));
+		status = -ENOMEM;
+		goto exit_main;
+	}
+
+	dir_entry_cnt = ns_read_dir_entries(instream, dir_entry_list);
+
+	if (dir_entry_cnt == 0) {
+		fprintf(stderr, "File not found\n");
+		status = -ENOENT;
+		goto exit_main;
+	}
+
+	/* Parse the command, and perform the requested action. */
+	if ((argc == 2) || (!strncasecmp(argv[2], "LI", 2))) {
+		printf("Filename  DA BLKS D TYP Type          Metadata\n");
+
+		for (i = 0; i < dir_entry_cnt; i++) {
+			ns_list_dir_entry(&dir_entry_list[i]);
+		}
+	} else {
+		if (argc < 4) {
+			printf("filename required.\n");
+			status = -1;
+			goto exit_main;
+		} else if (!strncasecmp(argv[2], "EX", 2)) {
+			for (i = 0; i < dir_entry_cnt; i++) {
+				result = ns_extract_file(&dir_entry_list[i], instream, argv[3]);
+				if (result == 0) {
+					extracted_file_count++;
+				}
+			}
+			printf("Extracted %d files.\n", extracted_file_count);
+		}
+	}
+
+exit_main:
+	if (dir_entry_list) free(dir_entry_list);
+
+	if (instream != NULL) fclose(instream);
+
+	return status;
+}
+
+int ns_read_dir_entries(FILE* stream, ns_dir_entry_t* dir_entries)
+{
+	size_t readlen;
+	int dir_entry_max = DIR_ENTRIES_SD;
+	int dd_flag;
+	int dir_entry_index = 0;
+	ns_dir_entry_t* dir_entry;
+
+	for (int i = 0; i < dir_entry_max; i++) {
+		dir_entry = &dir_entries[dir_entry_index];
+		readlen = fread(dir_entry, sizeof(ns_dir_entry_t), 1, stream);
+
+		if (readlen == 1) {
+			if (memcmp(dir_entry->sname, "        ", SNAME_LEN)) {
+
+				/* Bit 7 of the file type indicates double-density */
+				dd_flag = (dir_entry->file_type & DOUBLE_DENSITY_FLAG);
+
+				if (dd_flag) {
+					dir_entry->block_count *= 2;
+					dir_entry_max = DIR_ENTRIES_DD;
+				}
+
+				dir_entry_index++;
+			}
+		}
+	}
+	return dir_entry_index;
+}
+
+void ns_list_dir_entry(ns_dir_entry_t* dir_entry)
+{
+	char fname[SNAME_LEN + 1];
+	uint8_t file_type;
+	int dd_flag;
+
+	strncpy(fname, dir_entry->sname, sizeof(fname));
+	fname[SNAME_LEN] = '\0';
+	if (strncmp(fname, "        ", SNAME_LEN)) {
+
+		/* Bit 7 of the file type indicates double-density */
+		dd_flag = (dir_entry->file_type & DOUBLE_DENSITY_FLAG);
+		file_type = dir_entry->file_type & ~(DOUBLE_DENSITY_FLAG);
+
+		printf("%s %3d  %3d %c %3d ", fname, dir_entry->disk_address, dir_entry->block_count, dd_flag ? 'D' : ' ', file_type);
+
+		if (file_type < (sizeof(file_type_str) / sizeof(file_type_str[0]))) {
+			printf("%s", file_type_str[file_type]);
+		}
+		else {
+			printf("(Unknown)    ");
+		}
+
+		switch (file_type) {
+		case FILE_TYPE_OBJECT:
+			/* Binary object file, loaded with "GO," type-dependent data contains the load address */
+			printf(" Load addr: %04X\n", dir_entry->type_dependent_info[0] | (dir_entry->type_dependent_info[1] << 8));
+			break;
+		case FILE_TYPE_BASIC_SRC:
+			/* BASIC Source code, type-dependent data contains the actual program size in blocks */
+			printf(" Actual Size: %d\n", dir_entry->type_dependent_info[0]);
+			break;
+		default:
+			printf(" %02X,%02X,%02X\n", dir_entry->type_dependent_info[0], dir_entry->type_dependent_info[1], dir_entry->type_dependent_info[2]);
+			break;
+		}
+	}
+}
+
+int ns_extract_file(ns_dir_entry_t* dir_entry, FILE* instream, char *path)
+{
+	FILE* ostream;
+	int file_offset;
+	uint8_t* file_buf;
+	uint8_t file_type;
+	int file_len;
+	char fname[SNAME_LEN + 1];
+	char output_filename[32];
+	int dd_flag;
+
+	strncpy(fname, dir_entry->sname, sizeof(fname));
+	fname[SNAME_LEN] = '\0';
+
+	if (!strncmp(fname, "        ", SNAME_LEN)) {
+		return (-ENOENT);
+	}
+
+	/* Truncate the filename if a space is encountered. */
+	for (int j = 0; j < strlen(fname); j++) {
+		if (fname[j] == ' ') fname[j] = '\0';
+	}
+
+	/* Bit 7 of the file type indicates double-density */
+	dd_flag = (dir_entry->file_type & DOUBLE_DENSITY_FLAG);
+	file_type = dir_entry->file_type &= ~(DOUBLE_DENSITY_FLAG);
+
+	if ((dir_entry->block_count > 0) && (dir_entry->disk_address > 0)) {
+		file_len = dir_entry->block_count * NS_BLOCK_SIZE;
+
+		switch (file_type) {
+		case FILE_TYPE_DEFAULT:
+			snprintf(output_filename, sizeof(output_filename), "%s/%s.DEFAULT", path, fname);
+			break;
+		case FILE_TYPE_OBJECT:
+			snprintf(output_filename, sizeof(output_filename), "%s/%s.OBJECT_L%04X", path, fname,
+				dir_entry->type_dependent_info[0] | (dir_entry->type_dependent_info[1] << 8));
+			break;
+		case FILE_TYPE_BASIC_SRC:
+			file_len = dir_entry->type_dependent_info[0] * NS_BLOCK_SIZE;
+			snprintf(output_filename, sizeof(output_filename), "%s/%s.BASIC", path, fname);
+			break;
+		case FILE_TYPE_BASIC_DATA:
+			snprintf(output_filename, sizeof(output_filename), "%s/%s.BASIC_DATA", path, fname);
+			break;
+		default:
+			snprintf(output_filename, sizeof(output_filename), "%s/%s.TYPE_%d", path, fname,
+				file_type);
+			break;
+		}
+
+		output_filename[sizeof(output_filename) - 1] = '\0';
+		if (!(ostream = fopen(output_filename, "wb"))) {
+			printf("Error Openening %s\n", output_filename);
+			return (-ENOENT);
+		} else if (file_buf = (uint8_t*)calloc(1, file_len)) {
+			file_offset = dir_entry->disk_address * NS_BLOCK_SIZE * (dd_flag ? 2 : 1);
+			printf("%8s -> %s (%d bytes)\n", fname, output_filename, file_len);
+
+			fseek(instream, file_offset, SEEK_SET);
+			fread(file_buf, file_len, 1, instream);
+			fwrite(file_buf, file_len, 1, ostream);
+			free(file_buf);
+			fclose(ostream);
+			return (0);
+		} else {
+			printf("Memory allocation of %d bytes failed\n", file_len);
+			return (-ENOMEM);
+		}
+	}
+
+	return (-ENOENT);
+}
